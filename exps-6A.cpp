e312f3205e74fbd42527105f49a99384986aa0f7
saurabh-171204 <saurabhbihani5023@gmail.com>
2024-08-02 09:38:17 +0530

    Add files via upload

---
+++ exps-6A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int i,j,k=0,n;
+    cout<<"enter number of rows: ";
+    cin>>n;
+    for(i=1;i<=n;i++) {
+        for (j=1;j<=(n-1);j++){
+            cout<<"  ";
+
+        }
+        while (k!=(2*i-1)) {
+            cout<<" * ";
+            k++;
+        }
+        k=0;
+        cout<<endl;
+   }
+   cout<<endl;
+}
